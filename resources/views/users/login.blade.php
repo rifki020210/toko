2bd122c67d3a00e62e81fb230800644d426b1102
Muhamad Rifki Rahmadi <muhamadramadi335@gmail.com>
2023-12-14 08:23:18 +0700

    tinggal menampilkan pesan error pada input email

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -3,17 +3,17 @@
       <h3 class="card-title text-center mb-4">Login</h3>
        <form class="" method="post" action="/login">
         @csrf
-      <div class="form-floating">
-        <input type="email" class="form-control mb-3" id="email" placeholder="Email" name="email" value="{{ old('email') }}" autocomplete="off" required>
+      <div class="form-floating mb-3">
+        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="Email" name="email" value="{{ old('email') }}" autocomplete="off" required>
         <label for="email">Email</label>
-              @error('email')
+      @error('email')
         <div class="invalid-feedback">
           {{ $message }}
         </div>
       @enderror
       </div>
-      <div class="form-floating">
-    	   <input type="password" class="form-control mb-3" id="password" name="password" placeholder="password" required>
+      <div class="form-floating  mb-3">
+    	   <input type="password" class="form-control  @error('password') is-invalid @enderror" id="password" name="password" placeholder="password" required>
     	   <label for="password">Password</label>
       @error('password')
         <div class="invalid-feedback">
